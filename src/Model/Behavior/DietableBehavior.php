b835cd0df5deb4378b35805155d1279556a905c9
lsenft <lsenft@gmail.com>
2017-03-16 13:01:55 -0600

    empty behavior

---
+++ src/Model/Behavior/DietableBehavior.php
@@ -0,0 +1,12 @@
+<?php
+namespace DataDiet\Model\Behavior;
+
+use Cake\ORM\Behavior;
+
+class DietableBehavior extends Behavior
+{
+    public function initialize(array $config)
+    {
+        // Some initialization code here
+    }
+}
