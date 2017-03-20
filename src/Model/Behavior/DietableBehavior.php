baed6f4e5fb4c5c145bda91545ed3ca7f003eef4
lsenft <lsenft@gmail.com>
2017-03-20 14:46:22 -0600

    Change autoload

--- src/Model/Behavior/DietableBehavior.php
+++ src/Model/Behavior/DietableBehavior.php
@@ -2,11 +2,21 @@
 namespace Lsenft\DataDiet\Model\Behavior;
 
 use Cake\ORM\Behavior;
+use Cake\ORM\Query;
 
 class DietableBehavior extends Behavior
 {
     public function initialize(array $config)
     {
         // Some initialization code here
+        //die('test');
+        
+    }
+    
+    //public function beforeFind(Event $event, Query $query, ArrayObject $options, boolean $primary) {
+    public function beforeFind(Event $event, Query $query, ArrayObject $options, boolean $primary)
+    {
+        var_dump($options);
+       // die();
     }
 }
